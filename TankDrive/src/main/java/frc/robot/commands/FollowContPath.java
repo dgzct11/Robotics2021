998cf483d1e54dc76bafe1583d82df9415027a07
dgzct11 <dgzct11@icloud.com>
2021-08-31 08:31:31 -0500

    Started Wpilib Trajectory
    https://docs.wpilib.org/en/latest/docs/software/pathplanning/trajectory-tutorial/creating-drive-subsystem.html

--- TankDrive/src/main/java/frc/robot/commands/FollowContPath.java
+++ TankDrive/src/main/java/frc/robot/commands/FollowContPath.java
@@ -6,17 +6,27 @@ package frc.robot.commands;
 
 import java.util.ArrayList;
 
+import edu.wpi.first.wpilibj.controller.RamseteController;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import frc.robot.subsystems.NavXGyro;
 ;
 
 public class FollowContPath extends CommandBase {
   /** Creates a new FollowContPath. */
   double initialTime;
+  boolean firstTime = true;
+  double time = 0;
+  Trajectory trajectory;
+  RamseteController controller;
   public FollowContPath(int[] start, int[] end, int[][] points, double maxV, double maxA) {
     // Use addRequirements() here to declare subsystem dependencies.
     TrajectoryConfig config = new TrajectoryConfig(maxV, maxA);
@@ -26,22 +36,36 @@ public class FollowContPath extends CommandBase {
     for(int[] point: points)
       interiorWaypoints.add(new Translation2d(point[0], point[1]));
 
-    var trajectory = TrajectoryGenerator.generateTrajectory(
+    trajectory = TrajectoryGenerator.generateTrajectory(
       startPos, 
       interiorWaypoints, 
       endPos, 
       config);
-      initialTime = 0;
+      
+
+      controller = new RamseteController(2.0, 0.7);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
+    if(!firstTime){
+      time = System.currentTimeMillis()/1000F;
+      Trajectory.State goal = trajectory.sample(time);
+      Pose2d currentRobotPose = new Pose2d(NavXGyro.ahrs.getDisplacementX(), NavXGyro.ahrs.getDisplacementY(), Rotation2d.fromDegrees(NavXGyro.ahrs.getAngle()));
+      ChassisSpeeds adjustedSpeeds = controller.calculate(currentRobotPose, goal);
+      DifferentialDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(adjustedSpeeds);
+    } else {
+      initialTime = System.currentTimeMillis();
+      
+      firstTime = false;
+    }
   }
 
   // Called once the command ends or is interrupted.
